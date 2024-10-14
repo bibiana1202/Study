5bc27d6434f6e4b8f351b3173e5b7a78fdf22bbe
bibiana <105691874+bibiana1202@users.noreply.github.com>
2024-10-14 19:44:11 +0900

    241014 5장 배열

--- CodingTestStudy/src/chpater05/Problem03.java
+++ CodingTestStudy/src/chpater05/Problem03.java
@@ -1,16 +1,32 @@
 package chpater05;
+
 import java.util.ArrayList;
 // https://school.programmers.co.kr/learn/courses/30/lessons/68644
 import java.util.Arrays;
-
+import java.util.HashSet;
 
 public class Problem03 {
 	public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{2,1,3,4,1})));
-        System.out.println(Arrays.toString(solution(new int[]{5,0,2,7})));
+		System.out.println(Arrays.toString(solution(new int[] { 2, 1, 3, 4, 1 })));
+		System.out.println(Arrays.toString(solution(new int[] { 5, 0, 2, 7 })));
 	}
-	
-	private static int[] solution(int[] arr) {
+
+	public static int[] solution(int[] numbers) {
+		// 1. 중복값 제거를 위한 해시셋 생성
+		HashSet<Integer> set = new HashSet<>(); 
+
+		// 2. 두 수를 선택하는 모든 경우의 수를 반복문으로 구함
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				// 3. 두 수를 덯나 결과를 해시셋에 추가
+				set.add(numbers[i] + numbers[j]);
+			}
+		}
+
+		// 4. 해시셋의 값을 오름차순 정렬하고 int[] 형태의 배열로 변환하여 반환
+		return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+	}
+	private static int[] solution_hj(int[] arr) {
 		
 		ArrayList<Integer> list = new ArrayList<>();
 		
@@ -37,5 +53,4 @@ public class Problem03 {
 		
 	}
 }
-
 
