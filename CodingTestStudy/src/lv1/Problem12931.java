7e6ff168a7047481a6e6b2b075ba41fe32aa759a
bibiana <105691874+bibiana1202@users.noreply.github.com>
2024-10-18 08:41:11 +0900

    241017 프로그래머스 코딩테스트

---
+++ CodingTestStudy/src/lv1/Problem12931.java
@@ -0,0 +1,63 @@
+package lv1;
+
+public class Problem12931 {
+
+	
+	public class Solution {
+	    public int solution(int n) {
+	        int answer = 0;
+
+	        for(int i = 10 ; ;){
+			    
+	            int mok = n/i;
+	            int na  = n%i;
+	            answer += na/(i/10);
+	            if(mok<10){
+	                answer += mok;
+	                break;
+	            }
+	                
+	            i=i*10;
+	        }
+	        return answer;
+	    }
+	    
+	    public int solution_1(int n) {
+	        int answer = 0;
+
+	        while(true){
+	            answer+=n%10;
+	            if(n<10)
+	                break;
+
+	            n=n/10;
+	        }
+
+	        return answer;
+	    }
+	    
+	    public int solution_2(int n) {
+	        int answer = 0;
+	        String[] array = String.valueOf(n).split(""); // String 으로 형변환
+	        for(String s : array){
+	            answer += Integer.parseInt(s); //String 의 숫자를 int 타입으로 변환
+	        }
+	        return answer;
+	    }
+	    
+	    public int solution_3(int n) {
+	        int answer = 0;
+
+	        String str = Integer.toString(n);
+	        for (int i = 0; i < str.length(); i++) {
+	            answer += Integer.parseInt("" + str.charAt(i)); // String의 문자열에서 char 타입으로 변환
+	        }
+
+	        return answer;
+	    }
+	}
+	
+	
+	
+	
+}
